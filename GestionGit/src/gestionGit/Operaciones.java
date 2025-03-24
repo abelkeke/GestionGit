9972cb06a8494d59c115eb252277c6189be95030
abelfernandez <abelfernandez@localhost>
2025-03-24 19:13:10 +0100

    rama desarrollo: añadimos el metodo de potencia

--- GestionGit/src/gestionGit/Operaciones.java
+++ GestionGit/src/gestionGit/Operaciones.java
@@ -5,19 +5,25 @@ public class Operaciones {
 	public double sumar(double a, double b) {
 		return a + b;
 	}
-	
-	public int restar (int a,int b) {
-		if(a<b) return 0;
-		return a-b;
+
+	public int restar(int a, int b) {
+		if (a < b)
+			return 0;
+		return a - b;
 	}
-	
-	public double multiplicar (double a, double b) {
-		return a*b;
+
+	public double multiplicar(double a, double b) {
+		return a * b;
 	}
-	
-	public double dividir (double a,double b) {
-		if (b==0)return 0;
-		return a/b;
+
+	public double dividir(double a, double b) {
+		if (b == 0)
+			return 0;
+		return a / b;
 	}
-	
+
+	public double potencia(double base, double exponente) {
+		return Math.pow(base, exponente);
+	}
+
 }
