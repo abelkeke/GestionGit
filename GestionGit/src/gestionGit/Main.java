3f8b58abd3ed28a2ef9c5d3e217213655a5446be
abelfernandez <abelfernandez@localhost>
2025-03-24 18:58:34 +0100

    Modificamos clase Main con los metodos de Operaciones

--- GestionGit/src/gestionGit/Main.java
+++ GestionGit/src/gestionGit/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
 		System.out.println("Gestion con Git y GitHub");
 
+		Operaciones op = new Operaciones();
+		System.out.println("Suma: " + op.sumar(3.7, 4.1));
+		System.out.println("Multiplicacion: " + op.multiplicar(4, 7));
 	}
 
 }
