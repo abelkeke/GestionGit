1a30b1a79fd901f5c96e69d581739734e26c12f0
abelfernandez <abelfernandez@localhost>
2025-03-24 18:54:54 +0100

    Tercer commit: Modifico el metodo sumar para que sea con double

--- GestionGit/src/gestionGit/Operaciones.java
+++ GestionGit/src/gestionGit/Operaciones.java
@@ -2,11 +2,13 @@ package gestionGit;
 
 public class Operaciones {
 
-	public int sumar(int a, int b) {
+	public double sumar(int a, int b) {
 		return a + b;
 	}
 	
 	public int restar (int a,int b) {
 		return a-b;
 	}
+	
+	
 }
